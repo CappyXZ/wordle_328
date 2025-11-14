8f1f881d432dcd63551e73e115808dd1f1a051f7
Ben <bmiller04@ptd.net>
2025-11-14 09:31:01 -0500

    definition for client.py

--- client.py
+++ client.py
@@ -1,1 +1,26 @@
+###########################################################################################
+# Ben Miller, Cade Fandle, Gabe Sullivan
+# Computer Science
+# Created: November 14, 2025
+# Due: November 24, 2025
+# CSC 328
+# Prof Reiley Walther
+# Group Assignment - Wordle
+# client.py
+# Purpose: This is the client program for a networked version of Wordle. It connects 
+#          to a server, sends user guesses, and receives feedback on those guesses.
+# Language: Python
+# How to compile: python client.py < host name >
+###########################################################################################
 
+# imports
+import socket
+import sys
+
+def main():
+
+
+
+
+if __name__ == "__main__":
+    main()
